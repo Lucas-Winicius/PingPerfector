d7301d10200ca662db93b080027559ef0f2947ef
Lucas-Winicius <lucasclucas7@gmail.com>
2023-09-13 11:49:24 -0300

    "/user" - GET

--- src/routes/user/methods/index.ts
+++ src/routes/user/methods/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import Create from "./Create";
+import GetUser from "./Read";
 
 const routes = Router();
 
 routes.post("/user", Create);
+routes.get("/user", GetUser);
+routes.get("/user/:nick", GetUser);
 
 export default routes;
