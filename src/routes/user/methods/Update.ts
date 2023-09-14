3de7fd3c1537115c4c2c62415537d71b2fc403a2
Lucas-Winicius <lucasclucas7@gmail.com>
2023-09-14 11:47:47 -0300

    "/user" - PUT

---
+++ src/routes/user/methods/Update.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+async function Update(req: Request, res: Response) {
+  res.send("OPA");
+}
+
+export default Update;
--- src/routes/user/methods/index.ts
+++ src/routes/user/methods/index.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import Create from "./Create";
 import GetUser from "./Read";
+import Update from "./Update";
 
 const routes = Router();
 
 routes.post("/user", Create);
+
 routes.get("/user", GetUser);
 routes.get("/user/:nick", GetUser);
+
+routes.put("/user/:nick", Update);
 
 export default routes;
