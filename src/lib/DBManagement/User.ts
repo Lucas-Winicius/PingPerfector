cff5e563e1e454010fade7e4213aa532779441db
Lucas-Winicius <lucasclucas7@gmail.com>
2023-09-25 16:52:06 -0300

    database manager - Search user

--- src/lib/DBManagement/User.ts
+++ src/lib/DBManagement/User.ts
@@ -6,7 +6,7 @@ class UserDatabase {
   async createUser(data: UserTypes) {
     try {
       const response = await prisma.user.create({ data });
-      await redis.setEx(`user:${response.id}`, 7200, JSON.stringify(response));
+      redis.setEx(`user:${response.id}`, 7200, JSON.stringify(response));
 
       return {
         status: 201,
@@ -24,7 +24,44 @@ class UserDatabase {
   }
 
   async getUser(id: string | number) {
-    console.log(id);
+    const intId = Number(id);
+    try {
+      const cacheData = await redis.get(`user:${id}`);
+
+      if (cacheData) {
+        console.log("Encontrado em cache");
+        const user = JSON.parse(cacheData);
+        return {
+          status: 200,
+          message: "Great news! We've located the user you were looking for.",
+          data: user,
+        };
+      }
+
+      const user = await prisma.user.findFirst({ where: { id: intId } });
+      console.log("Buscando na base de dados");
+
+      if (!user) {
+        return {
+          status: 404,
+          message: "Sorry, we couldn't find the user you were looking for.",
+        };
+      }
+
+      redis.setEx(`user:${user.id}`, 7200, JSON.stringify(user));
+
+      return {
+        status: 200,
+        message: "The user's details have been successfully retrieved.",
+        data: user,
+      };
+    } catch (error) {
+      return {
+        status: 500,
+        message: "We encountered an issue while trying to fetch user data.",
+        error,
+      };
+    }
   }
 }
 
