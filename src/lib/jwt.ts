c8e62fb2ebcba02cf71ccc811f847d812d73cf97
Lucas-Winicius <lucasclucas7@gmail.com>
2023-10-01 19:07:07 -0300

    jwt verify

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,9 +1,19 @@
 import jwt from "jsonwebtoken";
+import UserTypes from "../@types/UserTypes";
 
 class JsonWebToken {
   create(data: string | object) {
     const string = jwt.sign(data, process.env.JWT_SECRET, { expiresIn: "7d" });
     return string;
+  }
+
+  verify(token: string): null | UserTypes {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET) as UserTypes;
+      return decoded;
+    } catch (err) {
+      return null;
+    }
   }
 }
 
